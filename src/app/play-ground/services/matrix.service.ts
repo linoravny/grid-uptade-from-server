4e1de48bd4d363311e2fd02c11bead5f5d7a0565
Linor <linoravny@gmail.com>
2021-07-24 13:44:24 +0300

    more fixes

--- src/app/play-ground/services/matrix.service.ts
+++ src/app/play-ground/services/matrix.service.ts
@@ -1,7 +1,6 @@
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, of, Subject } from 'rxjs';
-import { map, catchError } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { MatrixItem } from '../interfaces/matrix-item';
 
