3d109f5b0a40e0057422cde6a700690622034796
Linor <linoravny@gmail.com>
2021-07-24 13:28:18 +0300

    add icon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Matrix State App';
+  title = 'Grid Update From Server App';
 }
